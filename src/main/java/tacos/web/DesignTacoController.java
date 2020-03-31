d65446f0ec5a9618fde813be85e5a766792c0f1d
czq <15101620663@163.com>
2020-03-31 23:00:43 +0800

    Init commit.

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,6 +1,7 @@
 package tacos.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
+@Controller
 @RequestMapping("/design")
 public class DesignTacoController {
     @GetMapping
