f48fd8f994ff6cb18071bd1d5f38adcb9814ac33
czq <15101620663@163.com>
2020-04-01 23:44:32 +0800

    使用JdbcTemplate进行数据持久化

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -7,7 +7,9 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
 import tacos.Order;
+import tacos.data.OrderRepository;
 
 import javax.validation.Valid;
 
@@ -15,16 +17,24 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
+    private OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
     @GetMapping("/current")
-    public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
+    public String orderForm() {
         return "orderForm";
     }
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors) {
+    public String processOrder(@Valid Order order, Errors errors,
+                               SessionStatus sessionStatus) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
+        orderRepo.save(order);
+        sessionStatus.setComplete();
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
