779acd385088dc5daf159a245d4b673a8f7a0aee
czq <15101620663@163.com>
2020-04-02 20:12:54 +0800

    fix bug: 将jdbc API 替换为Spring.data.jdbc

--- src/main/java/tacos/TacosApplication.java
+++ src/main/java/tacos/TacosApplication.java
@@ -2,11 +2,9 @@ package tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class TacosApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TacosApplication.class, args);
     }
